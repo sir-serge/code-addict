cbb05bdb27b11d151fa2020063fa4236bb218912
sir-serge <irkzsrg@gmail.com>
2024-10-13 10:41:44 +0200

    add event listener

--- codeAddict.js
+++ codeAddict.js
@@ -1,3 +1,6 @@
 let hm=document.getElementById("hm");
 let tabs=document.getElementById("mobileTabs")
 let nav=document.getElementsByTagName("nav")
+hm.addEventListener("click",function(){
+    tabs.style.display='block';
+});
